b09cc237ca2abbbc216a0390f28f92d68bafb9ab
Moha2088 <113173091+Moha2088@users.noreply.github.com>
2024-04-23 12:58:39 +0200

    Intial Commit

---
+++ Builder/Entities/User.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Builder.Entities;
+public class User
+{
+    public int Id { get; set; }
+
+    public string Name { get; set; } = null!;
+
+    public string Email { get; set; } = null!;
+
+    public string Degree { get; set; } = null!;
+
+    public char Gender { get; set; }
+
+
+
+    public User(int Id, string Name, string Email, string Degree, char Gender)
+    {
+        this.Id = Id;
+        this.Name = Name;
+        this.Email = Email;
+        this.Degree = Degree;
+        this.Gender = Gender;
+    }
+
+    public class UserBuilder
+    {
+        private int Id;
+
+        private string Name;
+
+        private string Email;
+
+        private string Degree;
+
+        private char Gender;
+
+
+
+        public UserBuilder setId(int Id)
+        {
+            this.Id = Id;
+            return this;
+        }
+
+        public UserBuilder setName(string Name)
+        {
+            this.Name = Name;
+            return this;
+        }
+
+        public UserBuilder setEmail(string Email)
+        {
+            this.Email = Email;
+            return this;
+        }
+
+        public UserBuilder setDegree(string Degree)
+        {
+            this.Degree = Degree;
+            return this;
+        }
+
+        public UserBuilder setGender(char Gender)
+        {
+            this.Gender = Gender;
+            return this;
+        }
+
+        public User Build()
+        {
+            return new User(Id, Name, Email, Degree, Gender);
+        }
+    }
+}
---
+++ Builder/Program.cs
@@ -0,0 +1,90 @@
+﻿using System.Text;
+using Builder.Entities;
+using static Builder.Entities.User;
+
+namespace Builder;
+
+internal class Program
+{
+    static void Main(string[] args)
+    {
+        bool isRunning = true;
+
+        bool hasContents(string name, string email)
+        {
+            return !string.IsNullOrWhiteSpace(email) && !string.IsNullOrWhiteSpace(email);
+        }
+
+        List<User> userList = new();
+        while (isRunning)
+        {
+            try
+            {
+                Console.WriteLine("Press 1 to create a user, and 2 for userlist");
+                int input = int.Parse(Console.ReadLine());
+                switch (input)
+                {
+                    case 1:
+                    idPrompt:
+                        Console.WriteLine("Enter an id:");
+                        int id = int.Parse(Console.ReadLine());
+
+                        if (userList.Any(user => user.Id == id))
+                        {
+                            Console.WriteLine("That id already exist. Try a new one");
+                            goto idPrompt;
+                        }
+
+                        Console.WriteLine("Enter a name:");
+                        string? name = Console.ReadLine();
+                        Console.WriteLine("Enter an email:");
+                        string? email = Console.ReadLine();
+
+                        if (hasContents(name, email))
+                        {
+                            User user = new UserBuilder()
+                            .setId(id)
+                            .setName(name)
+                            .setEmail(email)
+                            .Build();
+
+                            userList.Add(user);
+                            Console.WriteLine($"User: {user.Name} has been created!");
+                        }
+
+                        else
+                        {
+                            Console.WriteLine("None of the fields can be empty!");
+                        }
+
+                        break;
+
+
+                    case 2:
+                        if (userList.Any())
+                        {
+                            StringBuilder dataBuilder = new StringBuilder();
+                            foreach (User user in userList)
+                            {
+                                dataBuilder.Append($"Id: {user.Id}\nName: {user.Name}\nEmail: {user.Email}\n{user.Gender}\n--------\n");
+                            }
+
+                            Console.WriteLine(dataBuilder);
+                        }
+
+                        else
+                        {
+                            Console.WriteLine("There are no registered users!");
+                        }
+
+                        break;
+                }
+            }
+
+            catch (Exception e)
+            {
+                Console.WriteLine($"Error: {e}");
+            }
+        }
+    }
+}
